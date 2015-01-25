65e1fba05278a23324303facf040efb94261443b
Jake Yoon <yjaeseok@github.com>
2015-01-26 05:17:48 +0900

    English Version add

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1949,3 +1949,15 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   })
 
 }(jQuery);
+
+
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement("style")
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      "@-ms-viewport{width:auto!important}"
+    )
+  )
+  document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
+}
